6c786a8f4b0f96a9f37facf3b6bb06295040b6ff
GeorgeCh <georgechirica92@gmail.com>
2020-08-23 14:11:48 +0300

    - renamed base to SSD1306_I2C

--- SSD1306_I2C.X/nbproject/Package-default.bash
+++ SSD1306_I2C.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Base_Config.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Base_Config.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=baseconfig.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SSD1306_I2C.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=SSD1306_I2C.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=ssd1306i2c.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/baseconfig.x/bin
+makeDirectory ${TMPDIR}/ssd1306i2c.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/baseconfig.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/ssd1306i2c.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/baseconfig.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/ssd1306i2c.x.tar *
 checkReturnCode
 
 # Cleanup
